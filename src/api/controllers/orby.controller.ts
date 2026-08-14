26329f3b441905ef1e1938a43ab5b1ecc3de003f
ilakkiyan-j <ilakkiyanj03@gmail.com>
2026-08-14 21:43:27 +0530

    Enhance webhook controller payload parsing and safe fallback handling for RASA metadata

--- src/api/controllers/orby.controller.ts
+++ src/api/controllers/orby.controller.ts
@@ -8,55 +8,60 @@ type RasaEntity = {
 };
 
 export const handleWebhook = async (req: Request, res: Response) => {
-    const actionName = req.body.next_action;
-    const metadata = req.body.tracker.latest_message.metadata;
+    const actionName = req.body?.next_action;
+    const latestMsg = req.body?.tracker?.latest_message || {};
+    const metadata = latestMsg.metadata || {};
+    const tracker = req.body?.tracker || {};
+
+    const shc_code = metadata.shc_code || tracker.sender_id || req.body?.sender_id || tracker.slots?.shc_code;
+    const qr_code = metadata.qr_code || tracker.slots?.qr_code;
+
     let responseText = "Sorry, I can't handle that action right now.";
 
     try {
         switch (actionName) {
             case 'action_find_hospital_visit':
-                let entitiesForHospitalVisit = req.body.tracker.latest_message.entities;
-                responseText = await orbyService.findPatientHospitalVisit(entitiesForHospitalVisit, metadata.shc_code, metadata.qr_code);
+                let entitiesForHospitalVisit = latestMsg.entities || [];
+                responseText = await orbyService.findPatientHospitalVisit(entitiesForHospitalVisit, shc_code, qr_code);
                 break;
             case 'action_find_last_record':
-                responseText = await orbyService.findPatientLastRecord(metadata.shc_code, metadata.qr_code);
+                responseText = await orbyService.findPatientLastRecord(shc_code, qr_code);
                 break;
             case 'action_find_last_hospital_visit':
-                responseText = await orbyService.findPatientLastHospitalVisit(metadata.shc_code, metadata.qr_code);
+                responseText = await orbyService.findPatientLastHospitalVisit(shc_code, qr_code);
                 break;
             case 'action_find_last_hospitalization':
-                responseText = await orbyService.findPatientLastHospitalization(metadata.shc_code, metadata.qr_code);
+                responseText = await orbyService.findPatientLastHospitalization(shc_code, qr_code);
                 break;
             case 'action_find_last_surgery':
-                responseText = await orbyService.findPatientLastSurgery(metadata.shc_code, metadata.qr_code);
+                responseText = await orbyService.findPatientLastSurgery(shc_code, qr_code);
                 break;
             case 'action_find_last_activity':
-                responseText = await orbyService.findPatientLastActivity(metadata.shc_code, metadata.qr_code);
+                responseText = await orbyService.findPatientLastActivity(shc_code, qr_code);
                 break;
             case 'action_check_allergy':
-                responseText = await orbyService.checkPatientAllergy(metadata.shc_code, metadata.qr_code);
+                responseText = await orbyService.checkPatientAllergy(shc_code, qr_code);
                 break;
             case 'action_check_habits':
-                responseText = await orbyService.checkPatientHabits(metadata.shc_code, metadata.qr_code);
+                responseText = await orbyService.checkPatientHabits(shc_code, qr_code);
                 break;
             case 'action_check_pregnancy':
-                responseText = await orbyService.checkPatientPregnancy(metadata.shc_code, metadata.qr_code);
+                responseText = await orbyService.checkPatientPregnancy(shc_code, qr_code);
                 break;
             case 'action_get_patient_overview':
-                responseText = await orbyService.getPatientOverview(metadata.shc_code, metadata.qr_code);
+                responseText = await orbyService.getPatientOverview(shc_code, qr_code);
                 break;
             case 'action_get_contact_info':
-                responseText = await orbyService.getPatientContactInfo(metadata.shc_code, metadata.qr_code);
+                responseText = await orbyService.getPatientContactInfo(shc_code, qr_code);
                 break;
             case 'action_find_past_diagnoses':
-                responseText = await orbyService.findPatientPastDiagnoses(metadata.shc_code, metadata.qr_code);
+                responseText = await orbyService.findPatientPastDiagnoses(shc_code, qr_code);
                 break;
             case 'action_find_current_medications':
-                responseText = await orbyService.findPatientCurrentMedications(metadata.shc_code, metadata.qr_code);
+                responseText = await orbyService.findPatientCurrentMedications(shc_code, qr_code);
                 break;
             case 'action_find_doctor_visit':
-                let entitiesForDoctorVisit: RasaEntity[] = req.body.tracker.latest_message.entities || [];
-                console.log(entitiesForDoctorVisit);
+                let entitiesForDoctorVisit: RasaEntity[] = latestMsg.entities || [];
                 if (entitiesForDoctorVisit.length > 1) {
                     const combinedName = entitiesForDoctorVisit.map(e => e.value).join(' ');
                     entitiesForDoctorVisit = [{
@@ -64,35 +69,35 @@ export const handleWebhook = async (req: Request, res: Response) => {
                         value: combinedName
                     }];
                 }
-                responseText = await orbyService.findPatientDoctorVisit(entitiesForDoctorVisit, metadata.shc_code, metadata.qr_code);
+                responseText = await orbyService.findPatientDoctorVisit(entitiesForDoctorVisit, shc_code, qr_code);
                 break;
             case 'action_find_lab_results':
-                const labResultsEntities = req.body.tracker.latest_message.entities || [];
-                responseText = await orbyService.findPatientLabResults(labResultsEntities, metadata.shc_code, metadata.qr_code);
+                const labResultsEntities = latestMsg.entities || [];
+                responseText = await orbyService.findPatientLabResults(labResultsEntities, shc_code, qr_code);
                 break;
             case 'action_find_emergency_contact':
-                const emgEntities = req.body.tracker.latest_message.entities || [];
-                responseText = await orbyService.findPatientEmergencyContact(emgEntities, metadata.shc_code, metadata.qr_code);
+                const emgEntities = latestMsg.entities || [];
+                responseText = await orbyService.findPatientEmergencyContact(emgEntities, shc_code, qr_code);
                 break;
             case 'action_get_health_tip':
-                const healthTipEntities = req.body.tracker.latest_message.entities || [];
+                const healthTipEntities = latestMsg.entities || [];
                 responseText = await orbyService.getHealthTip(healthTipEntities);
                 break;
             case 'action_find_specialist':
-                const doctorEntities = req.body.tracker.latest_message.entities || [];
+                const doctorEntities = latestMsg.entities || [];
                 responseText = await orbyService.findSpecialistDoctor(doctorEntities);
                 break;
             case 'action_find_hospital':
-                const findHospitalEntities = req.body.tracker.latest_message.entities || [];
+                const findHospitalEntities = latestMsg.entities || [];
                 responseText = await orbyService.findHospital(findHospitalEntities);
                 break;
             case 'action_get_record_count':
-                const recordEntities = req.body.tracker.latest_message.entities || [];
-                responseText = await orbyService.getRecordCount(recordEntities, metadata.shc_code, metadata.qr_code);
+                const recordEntities = latestMsg.entities || [];
+                responseText = await orbyService.getRecordCount(recordEntities, shc_code, qr_code);
                 break;
             case 'action_find_treatments_for_diagnosis':
-                const findTreatmentEntities = req.body.tracker.latest_message.entities || [];
-                responseText = await orbyService.findTreatmentsForDiagnosis(findTreatmentEntities, metadata.shc_code, metadata.qr_code);
+                const findTreatmentEntities = latestMsg.entities || [];
+                responseText = await orbyService.findTreatmentsForDiagnosis(findTreatmentEntities, shc_code, qr_code);
                 break;
             // Add other cases here...
         }
